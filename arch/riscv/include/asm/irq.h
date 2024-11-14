3f06adc49846118d03ed9fa80d027c26188fcb9d
Andrew Jones <ajones@ventanamicro.com>
2024-11-14 17:18:56 +0100

    RISC-V: Define irqbypass vcpu_info
    The vcpu_info parameter to irq_set_vcpu_affinity() effectively
    defines an arch specific IOMMU <=> hypervisor protocol. Provide
    a definition for the RISCV IOMMU.
    
    Signed-off-by: Andrew Jones <ajones@ventanamicro.com>
    Link: https://lore.kernel.org/r/20241114161845.502027-28-ajones@ventanamicro.com
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/riscv/include/asm/irq.h
+++ arch/riscv/include/asm/irq.h
@@ -25,6 +25,15 @@ struct fwnode_handle *riscv_get_intc_hwnode(void);
 int riscv_get_hart_index(struct fwnode_handle *fwnode, u32 logical_index,
 			 u32 *hart_index);
 
+struct riscv_iommu_vcpu_info {
+	u64 msi_addr_pattern;
+	u64 msi_addr_mask;
+	u32 group_index_bits;
+	u32 group_index_shift;
+	u64 gpa;
+	u64 hpa;
+};
+
 #ifdef CONFIG_ACPI
 
 enum riscv_irqchip_type {
